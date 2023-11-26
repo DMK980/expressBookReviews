55fb43442625637544a7631dab2f021a5c28004e
Your Name <you@example.com>
2023-11-25 23:25:41 -0500

    stopped at task 3

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -12,14 +12,21 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let bookTitles = {}
+  for (let i = 1; i < 11;i++){
+      bookTitles[i]= books[i]["title"]
+  }
+  return res.status(300).send(JSON.stringify(bookTitles,null,4));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let isbn = Number(req.params.isbn);
+  if (isbn > 10 || isbn < 1){
+      return res.status(400).json({message:"Enter an isbn between 1 - 10"});
+  } else {
+      return res.status(200).send(JSON.stringify(books[isbn]));
+  }
  });
   
 // Get book details based on author
