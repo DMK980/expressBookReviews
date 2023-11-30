5c8350f0ce87b00681bce13edd0b946e8c041a37
DMK980 <derrick.mutuma@gmail.com>
2023-11-30 07:44:30 -0500

    task 8

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -54,8 +54,23 @@ regd_users.post("/login", (req,res) => {
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  
+  let username = req.body.username;
+  let isbn = req.params.isbn;
+  let review = req.query.review;
+
+  for (let key in books){
+
+      if (key === isbn){
+
+        books[key].review[`${username}`] = review
+
+      }
+
+  }
+
+  return res.status(200).json({message: "Book Review added"});
+
 });
 
 module.exports.authenticated = regd_users;
